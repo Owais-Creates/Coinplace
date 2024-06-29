22251c1caa0a12af49cb0163c67f6930f869ca37
Owais Ali <zaidalit00@gmail.com>
2024-06-29 19:48:30 +0530

    added the modal component

--- crypto/src/pages/Home/Home.jsx
+++ crypto/src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react'
 import './Home.css'
 import { useCoin } from '../../context/CoinContext'
 import { Link } from 'react-router-dom'
+import modal from "../../assets/modal.jpg"
+import { ImCross } from "react-icons/im";
+
 
 const Home = () => {
 
   const { allCoin, currency } = useCoin();
   const [displayCoin, setDisplayCoin] = useState([]);
   const [input, setInput] = useState("");
+  const [isModalActive, setIsModalActive] = useState(false);
 
   const handleInput = (e) => {
     setInput(e.target.value);
@@ -31,14 +35,25 @@ const Home = () => {
     }
   };
 
+  const handleModal = () => {
+    setIsModalActive(false)
+  }
 
   useEffect(() => {
-    setDisplayCoin(allCoin);
+    setDisplayCoin(allCoin)
   }, [allCoin])
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setIsModalActive(true)
+    }, 3000)
+
+  }, [])
 
   return (
     <>
-      {displayCoin
+      {!isModalActive
         ?
         <div className="home">
           <div className="hero">
@@ -68,7 +83,7 @@ const Home = () => {
             </div>
 
             {
-              displayCoin.slice(0,50).map((item, index) => (
+              displayCoin.slice(0, 50).map((item, index) => (
                 <Link to={`/coin/${item.id}`} key={index} className='table-layout' >
                   <p>{item.market_cap_rank}</p>
                   <div>
@@ -83,12 +98,29 @@ const Home = () => {
               ))
             }
           </div>
-          </div>
+        </div>
 
         :
-        <div className="spinner">
-          <div className="spin"></div>
+        <div onClick={handleModal} className="modal-container">
+
+          <div className="modal">
+
+            <div className='modal-img-container' >
+              <img src={modal} alt="" />
+              <div onClick={handleModal} >
+                <ImCross className='cross-icon' />
+              </div>
+            </div>
+
+            <div className='modal-paragraph' >
+              <h1>Welcome to the Coinplace</h1>
+              <p>Your all in one cryptocurrency place to find all the information about the coins. </p>
+            </div>
+
+          </div>
+
         </div>
+
       }
     </>
   )
